114779a097139bec7313b081debfda738b9eb5f6
toasted226 <51060273+toasted226@users.noreply.github.com>
2022-06-06 19:49:22 +0200

    Initial commit

---
+++ Brainfrick/PlankBrainfrick/PlankBrainfrick/ImageParser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PlankBrainfrick
+{
+    public class ImageParser
+    {
+        public string path { get; set; }
+
+        public ImageParser(string imagePath) 
+        {
+            path = imagePath;
+        }
+        
+        public string[] GetPixels() 
+        {
+            List<string> pixels = new List<string>();
+
+            if (File.Exists(path))
+            {
+                var bitmap = new Bitmap(path);
+
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        var pixel = bitmap.GetPixel(y, x);
+                        string pixelData = ColorTranslator.ToHtml(pixel);
+                        pixels.Add(pixelData);
+                    }
+                }
+            }
+            else 
+            {
+                throw new FileNotFoundException("Could not find a file at path " + path);
+            }
+
+            return pixels.ToArray();
+        }
+
+        public string GetCommandBuffer()
+        {
+            string[] pixels = GetPixels();
+            string commandBuffer = "";
+
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                switch (pixels[i])
+                {
+                    case "#000000":
+                        commandBuffer += ".";
+                        break;
+                    case "#888888":
+                        commandBuffer += ",";
+                        break;
+                    case "#0000FF":
+                        commandBuffer += "+";
+                        break;
+                    case "#FF0000":
+                        commandBuffer += "-";
+                        break;
+                    case "#FF00FF":
+                        commandBuffer += "<";
+                        break;
+                    case "#00FF00":
+                        commandBuffer += ">";
+                        break;
+                    case "#00FFCC":
+                        commandBuffer += "[";
+                        break;
+                    case "#FF8800":
+                        commandBuffer += "]";
+                        break;
+                }
+            }
+            
+            return commandBuffer;
+        }
+    }
+}
---
+++ Brainfrick/PlankBrainfrick/PlankBrainfrick/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace PlankBrainfrick
+{
+	class MainClass
+	{
+        //public static string textpath = "C:/Users/Keagan/Documents/Plank/Brainfrick/code.txt";
+		public static string imagepath =
+			"C:/Users/Keagan/Documents/Plank/Brainfrick/Scripts/helloworld_disorganised_16.png";
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Getting Command Buffer...");
+            /*
+			string commandBuffer = File.ReadAllText(textpath).Trim().Replace("\n", string.Empty);
+            commandBuffer = commandBuffer.Replace(" ", string.Empty);
+            */
+            ImageParser imageParser = new ImageParser(imagepath);
+            string commandBuffer = imageParser.GetCommandBuffer();
+            Console.WriteLine("Command buffer:\n" + commandBuffer + "\n");
+
+            RunCode(commandBuffer);
+        }
+
+		public static void RunCode(string commandBuffer) 
+		{
+			Console.WriteLine("Output:\n");
+
+			int[] memory = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+			int memoryPointer = 0;
+			string result = "";
+
+			for (int i = 0; i < commandBuffer.Length; ++i)
+			{
+				if (commandBuffer[i] == ',')
+				{
+					Console.Clear();
+					Console.Write("Provide input: ");
+					string input = Console.ReadLine();
+					memory[memoryPointer] = Convert.ToInt32(input);
+				}
+				else if (commandBuffer[i] == '.')
+				{
+					Console.Write(Convert.ToChar(memory[memoryPointer]));
+					result += Convert.ToChar(memory[memoryPointer]);
+				}
+				else if (commandBuffer[i] == '<')
+				{
+					memoryPointer--;
+				}
+				else if (commandBuffer[i] == '>')
+				{
+					memoryPointer++;
+				}
+				else if (commandBuffer[i] == '+')
+				{
+					memory[memoryPointer] += 1;
+				}
+				else if (commandBuffer[i] == '-')
+				{
+					memory[memoryPointer] -= 1;
+				}
+				else if (commandBuffer[i] == '[')
+				{
+					if (memory[memoryPointer] == 0)
+					{
+						int skip = 0;
+						int ptr = i + 1;
+						while (commandBuffer[ptr] != ']' || skip > 0)
+						{
+							if (commandBuffer[ptr] == '[')
+							{
+								skip += 1;
+							}
+							else if (commandBuffer[ptr] == ']')
+							{
+								skip -= 1;
+							}
+							ptr += 1;
+							i = ptr;
+						}
+					}
+				}
+				else if (commandBuffer[i] == ']')
+				{
+					if (memory[memoryPointer] != 0)
+					{
+						int skip = 0;
+						int ptr = i - 1;
+						while (commandBuffer[ptr] != '[' || skip > 0)
+						{
+							if (commandBuffer[ptr] == ']')
+							{
+								skip += 1;
+							}
+							else if (commandBuffer[ptr] == '[')
+							{
+								skip -= 1;
+							}
+							ptr -= 1;
+							i = ptr;
+						}
+					}
+				}
+			}
+
+            Console.WriteLine("\n\nProgram finished.");
+        }
+	}
+}
